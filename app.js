ea370bd82b9f77d53a61d5ca493c42c01af44836
Servin Maria <106326633+Mery347@users.noreply.github.com>
2022-10-27 22:41:51 -0300

    subo formularios login y register tp finalizado

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const path = require("path")
-const app = express()
-const port = 3000
+const express = require ('express');
+const app = express();
+const path = require("path");
+const port = 3000;
 
-app.use(express.static(path.resolve(__dirname,'public')))
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
-app.listen(port,()=>console.log(`Se levanto con exito http://localhost:${port}`))
+
+app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"views","home.html")));
+app.get("/register",(req,res)=>res.sendFile(path.resolve(__dirname,"views","register.html")));
+app.get("/login",(req,res)=>res.sendFile(path.resolve(__dirname,"views","login.html")));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(port, ()=> console.log(`Server rise in http://localhost:${port}`))
